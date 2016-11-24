bcf915290b6cc2096a07ae3b0be2d9cbcc7d24d6
leinue <597055914@qq.com>
2016-11-24 20:59:18 +0800

    update

--- src/components/RightSidebar/Controllers.js
+++ src/components/RightSidebar/Controllers.js
@@ -14,20 +14,46 @@ const ConstructionTree = () => {
 
 	}
 
-  return (
+  	return (
 
-  	<div>
 	    <Row>
-	      <Col span={12}>
+	      	<Col span={12}>
+	      		<div className="app-components button-bar"><span className="title">按钮组</span></div>
+	      	</Col>
 
-	      	
+	      	<Col span={12}>
+	      		<div className="app-components button"><span className="title">按钮</span></div>
+	      	</Col>
 
-	      </Col>
-	      <Col span={12}>col-12</Col>
+	      	<Col span={12}>
+	      		<div className="app-components card"><span className="title">按钮组</span></div>
+	      	</Col>
+
+	      	<Col span={12}>
+	      		<div className="app-components header"><span className="title">按钮组</span></div>
+	      	</Col>
+
+	      	<Col span={12}>
+	      		<div className="app-components checkbox"><span className="title">选择框</span></div>
+	      	</Col>
+
+	      	<Col span={12}>
+	      		<div className="app-components form"><span className="title">按钮组</span></div>
+	      	</Col>
+
+	      	<Col span={12}>
+	      		<div className="app-components footer"><span className="title">按钮组</span></div>
+	      	</Col>
+
+	      	<Col span={12}>
+	      		<div className="app-components heading"><span className="title">按钮组</span></div>
+	      	</Col>
+
 	    </Row>
-  	</div>
 
-  );
+
+
+  	);
 
 };
 
