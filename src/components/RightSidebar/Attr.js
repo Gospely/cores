dd540d1aa56aa654aae70650729f2d1b17922336
leinue <597055914@qq.com>
2016-11-25 09:16:47 +0800

    update

--- src/components/RightSidebar/Attr.js
+++ src/components/RightSidebar/Attr.js
@@ -15,52 +15,8 @@ const Attr = (props) => {
 	var onCheck = function() {
 
 	}
-
-	const residences = [{
-	  value: 'zhejiang',
-	  label: 'Zhejiang',
-	  children: [{
-	    value: 'hangzhou',
-	    label: 'Hangzhou',
-	    children: [{
-	      value: 'xihu',
-	      label: 'West Lake',
-	    }],
-	  }],
-	}, {
-	  value: 'jiangsu',
-	  label: 'Jiangsu',
-	  children: [{
-	    value: 'nanjing',
-	    label: 'Nanjing',
-	    children: [{
-	      value: 'zhonghuamen',
-	      label: 'Zhong Hua Men',
-	    }],
-	  }],
-	}];
-
-    const { getFieldDecorator } = this.props.form;
-    const formItemLayout = {
-      labelCol: { span: 6 },
-      wrapperCol: { span: 14 },
-    };
-    const tailFormItemLayout = {
-      wrapperCol: {
-        span: 14,
-        offset: 6,
-      },
-    };
-    const prefixSelector = getFieldDecorator('prefix', {
-      initialValue: '86',
-    })(
-      <Select className="icp-selector">
-        <Option value="86">+86</Option>
-      </Select>
-    );
-
 	return (
-
+		<div></div>
 	);
 
 };
