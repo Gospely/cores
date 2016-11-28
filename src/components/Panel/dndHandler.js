c218c336311f118479f31bf8dc29f636c29ce1ff
leinue <597055914@qq.com>
2016-11-28 10:26:52 +0800

    update

---
+++ src/components/Panel/dndHandler.js
@@ -0,0 +1,58 @@
+
+export default {
+	init(props) {
+		this.props = props;
+
+		window.addEventListener("message", (event) =>  {
+			console.log('previewPage receives message', event);
+
+			const evtAction = {
+
+				ctrlClicked () {
+
+
+
+				},
+
+				ctrlDragging () {
+
+				},
+
+				ctrlDragover () {
+
+				},
+
+				ctrlRefreshed () {
+
+				},
+
+				ctrlAdded () {
+
+					props.dispatch({
+						type: 'rightbar/setActiveMenu',
+						payload: 'attr'
+					});
+
+					props.dispatch({
+						type: 'designer/addController',
+						payload: dndData
+					});
+					
+				    props.dispatch({
+				        type: 'attr/setFormItemsByDefault'
+				    });
+
+				},
+
+				ctrlRemoved () {
+					
+				}
+
+			}
+
+			evtAction[event]();
+		});
+	},
+
+
+}
