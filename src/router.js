65c14beaece42191abb16446c4d23e9f3083edce
SharksevenRo <937257166@qq.com>
2016-12-06 10:14:56 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import React, { PropTypes } from 'react';
 import { Router, Route, IndexRoute, Link } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import auth from './utils/auth';
 
 import Products from './routes/Products';
 
 export default function({ history }) {
+
+  auth(history);
   return (
     <Router history={history}>
       	<Route path="/" component={IndexPage}>
