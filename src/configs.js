3d95cc846ebe392a33987fc6d7627a8017b4d2ad
leinue <597055914@qq.com>
2016-12-28 09:35:15 +0800

    update

--- src/configs.js
+++ src/configs.js
@@ -1,6 +1,6 @@
 export default {
-	//baseURL: 'http://api.gospely.com/',
+	baseURL: 'http://api.gospely.com/',
 	//baseURL: 'http://localhost:8089/'
-	//baseURL: 'http://api.gospely.com:9999/'
-	baseURL: 'http://192.168.31.23:9999/'
+	// baseURL: 'http://api.gospely.com:9999/'
+	// baseURL: 'http://192.168.31.23:9999/'
 }
