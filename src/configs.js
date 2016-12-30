e14b8f36d255bd08c1aae99a0f3111e750779f70
SharksevenRo <937257166@qq.com>
2016-12-30 13:38:33 +0800

    update

--- src/configs.js
+++ src/configs.js
@@ -1,11 +1,8 @@
 export default {
 	//baseURL: 'http://api.gospely.com/',
 	//baseURL: 'http://120.76.235.234:9999/'
-<<<<<<< HEAD
 	baseURL: 'http://localhost:8089/'
-=======
 	// baseURL: 'http://localhost:8089/'
->>>>>>> 9beed1ee46ccb79943539adacee01f82a47ecbba
 	//baseURL: 'http://api.gospely.com:9999/'
 	//baseURL: 'http://' + localStorage.host + '/',
 	// baseURL: 'http://192.168.31.23:9999/'
