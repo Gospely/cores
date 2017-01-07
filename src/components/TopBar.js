cd02f74e881062ad9dc694860dc94bd0791a0082
SharksevenRo <937257166@qq.com>
2017-01-07 15:14:57 +0800

    update

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -281,9 +281,9 @@ const LeftSidebar = (props) => {
 	        showStartMenu() {
 
 				if(localStorage.debugType == 'common'){
-					sessionStorage.currentDebugResource = 'http://'+ localStorage.host +':' + localStorage.port;
+					sessionStorage.currentDebugResource = 'http://'+ localStorage.domain + '.gospely.com';
 					// var debug = window.open(location.origin + '/static/debugger/wordpress.html','_blank')
-					var debug = window.open('http://'+ localStorage.host +':' + localStorage.port);
+					var debug = window.open('http://'+ localStorage.domain + '.gospely.com');
 					// props.dispatch({
 					// 	type: 'devpanel/handleDebugger',
 					// 	payload: {debug}
@@ -574,7 +574,7 @@ const LeftSidebar = (props) => {
 					common(){
 						sessionStorage.currentDebugResource = 'http://' + localStorage.domain;
 						// var debug = window.open(location.hostname + '/static/debugger/wordpress.html','_blank')
-						window.open('http://' + localStorage.domain;);
+						window.open('http://' + localStorage.domain);
 						// props.dispatch({
 						// 	type: 'devpanel/handleDebugger',
 						// 	payload: {debug}
@@ -615,7 +615,7 @@ const LeftSidebar = (props) => {
 		localStorage.debugType == 'shell' &&
 		<Menu onClick={onSelectStartMenu}>
 			<Menu.Item key='runCommand' disabled={window.disabled}>运行：{props.sidebar.debugConfig.runCommand}</Menu.Item>
-			<Menu.Item key='visit' disabled={window.disabled}>访问：http://{localStorage.host}:{localStorage.port}</Menu.Item>
+			<Menu.Item key='visit' disabled={window.disabled}>访问：http://{localStorage.domain}</Menu.Item>
 			<Menu.Item key='run&visit&noleave' disabled={window.disabled}>在IDE访问</Menu.Item>
 			<Menu.Divider/>
 			<Menu.Item key='config' disabled={window.disabled}>配置...</Menu.Item>
