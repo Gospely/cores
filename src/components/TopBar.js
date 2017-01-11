282260593222a01db70db998350affcda8697f17
SharksevenRo <937257166@qq.com>
2017-01-11 19:01:18 +0800

    update

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1360,7 +1360,6 @@ const LeftSidebar = (props) => {
 		        	type: 'devpanel/add',
 	    	    	payload: { title, type }
 	      		});
-			console.log( props.sidebar.modalCommitInfo.title);
 			props.dispatch({
 				type: 'devpanel/initDebugPanel',
 				payload: { cmd: 'cd /root/workspace\n clear && git commit -a -m "' + props.sidebar.modalCommitInfo.title + '"\n' }
