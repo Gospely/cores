01f520ca872b8d544bcba0d9813bae9ecf9731a7
SharksevenRo <937257166@qq.com>
2017-01-15 12:35:08 +0800

    update

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -450,12 +450,17 @@ const LeftSidebar = (props) => {
 				message: '正在删除应用，请稍等……',
 				title: '删除应用'
 			});
-
+			if(localStorage.applicationId == application){
+				localStorage.clear;
+				window.location.hash = '#/'
+				props.dispatch({
+					type: 'devpanel/initPanel',
+				})
+			}
 			props.dispatch({
 				type: 'sidebar/deleteApp',
 				payload: {application}
 			})
-			// window.location.href = window.location.origin;
 	    },
 
 	    cancelDeleteApp() {
@@ -644,6 +649,12 @@ const LeftSidebar = (props) => {
 		},
 
 		createAppFromModal() {
+			props.dispatch({
+				type: 'sidebar/handleAvailable',
+				payload: {
+					available: true,
+				}
+			});
 			props.dispatch({
 				type: 'sidebar/showNewAppAndHideSwitch',
 			})
