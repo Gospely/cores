237fd4b4544f2b59a96e69dbb142f4652aa7b005
SharksevenRo <937257166@qq.com>
2017-01-18 13:25:08 +0800

    update

--- src/utils/fileListen.js
+++ src/utils/fileListen.js
@@ -66,45 +66,6 @@ const fileListen = function (props, namespace) {
 	});
 	window.fileSocket = socket;
 
-	// socket.emit('join listen',{
-    //     user_id : localStorage.user
-    // });
-	//
-	//
-	// socket.on('connect_failed', function() {
-	// 	// alert('失败')
-	// })
-	//
-	// socket.on('error', function() {
-	// 	// alert('错误')
-	// })
-	//
-	// socket.on('connecting', function() {
-	// 	console.log('正在连接')
-	// })
-	//
-	// socket.on('join', function(ev) {
-	// 	console.lgo(ev)
-	// })
-	// socket.onopen = function() {
-	// 	console.log('文件监听打开')
-	// };
-	// socket.onclose = function() {
-	// 	console.log('文件监听关闭')
-	// };
-	// socket.onerror = function() {
-	// 	console.log('文件监听错误')
-	// };
-	// socket.onmessage = function (evt) {
-	//   //收到服务器消息，使用evt.data提取
-	// console.log(evt.data);
-	//   	if(/^\{[\s*"\w+":"\w+",*\s*]+\}$/.test(evt.data)){
-	// 		console.log(evt.data);
-	// 		var data = JSON.parse(evt.data);
-	// 		console.log(JSON.parse(evt.data));
-	//   	}
-	// };
-
 }
 
 export default fileListen;
