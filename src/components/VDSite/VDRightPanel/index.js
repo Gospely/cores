afdc9cf5e8b7a1c441af52ba6b4a9fc5ab36ade9
leinue <597055914@qq.com>
2017-02-08 19:37:47 +0800

    update

--- src/components/VDSite/VDRightPanel/index.js
+++ src/components/VDSite/VDRightPanel/index.js
@@ -3,6 +3,15 @@ import { connect } from 'dva';
 
 import { Button, Modal } from 'antd';
 import { Tabs, Icon } from 'antd';
+import { Tooltip } from 'antd';
+
+import VDAssets from './VDAssets.js';
+import VDControllerList from './VDControllerList.js';
+import VDInteractions from './VDInteractions.js';
+import VDSettingPanel from './VDSettingPanel.js';
+import VDStyleManager from './VDStyleManager.js';
+import VDStylePanel from './VDStylePanel.js';
+
 const TabPane = Tabs.TabPane;
 
 const VDRightPanel = (props) => {
@@ -10,12 +19,48 @@ const VDRightPanel = (props) => {
   return (
     <div className="vd-right-panel">
       <Tabs defaultActiveKey="styles">
-        <TabPane tab={<Icon type="edit" />} key="styles">Content of tab 1</TabPane>
-        <TabPane tab={<Icon type="setting" />} key="settings">Content of tab 2</TabPane>
-        <TabPane tab={<Icon type="bars" />} key="controllers">Content of tab 3</TabPane>
-        <TabPane tab={<Icon type="exception" />} key="styles-manager">Content of tab 4</TabPane>
-        <TabPane tab={<Icon type="switcher" />} key="interactions">Content of tab 5</TabPane>
-        <TabPane tab={<Icon type="picture" />} key="assets">Content of tab 6</TabPane>
+
+        <TabPane tab={
+          <Tooltip placement="bottom" title="样式">
+            <Icon type="edit" />
+          </Tooltip>} key="styles">
+          <VDStylePanel></VDStylePanel>
+        </TabPane>
+
+        <TabPane tab={
+          <Tooltip placement="bottom" title="组件设置">
+            <Icon type="setting" />
+          </Tooltip>} key="settings">
+          <VDSettingPanel></VDSettingPanel>
+        </TabPane>
+
+        <TabPane tab={
+          <Tooltip placement="bottom" title="组件树">
+            <Icon type="bars" />
+          </Tooltip>} key="controllers">
+          <VDControllerList></VDControllerList>
+        </TabPane>
+
+        <TabPane tab={
+          <Tooltip placement="bottom" title="样式管理">
+            <Icon type="exception" />
+          </Tooltip>} key="styles-manager">
+          <VDStyleManager></VDStyleManager>
+        </TabPane>
+
+        <TabPane tab={
+          <Tooltip placement="bottom" title="交互动画">
+            <Icon type="inbox" />
+          </Tooltip>} key="interactions">
+          <VDInteractions></VDInteractions>
+        </TabPane>
+
+        <TabPane tab={
+          <Tooltip placement="bottom" title="资源文件">
+            <Icon type="picture" />
+          </Tooltip>} key="assets">
+          <VDAssets></VDAssets>
+        </TabPane>
       </Tabs>
     </div>
   );
