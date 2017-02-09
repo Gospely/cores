2afa196523013cc291c7cca01b51ff87b4be98c6
leinue <597055914@qq.com>
2017-02-09 11:00:56 +0800

    updat

--- src/components/VDSite/VDLeftPanel/VDAllPages.js
+++ src/components/VDSite/VDLeftPanel/VDAllPages.js
@@ -29,12 +29,14 @@ const Component = (props) => {
 
 	}
 
-  const pageDetailSettings = (
-    <div>
-      <p>Content</p>
-      <p>Content</p>
-    </div>
-  );
+  const generatePageDetailSettings = () => {
+    return (
+      <div>
+        <p>Content</p>
+        <p>Content</p>
+      </div>
+    );
+  };
 
 	return (
     <Menu onClick={allPagesProps.handlePageListItemClick}
@@ -50,36 +52,73 @@ const Component = (props) => {
           </Col>
           <Col span={4}>
             <Tooltip placement="top" title="设置页面的详细信息">
-              <Popover placement="right" title="设置页面的详细信息" content={pageDetailSettings} trigger="click">
+              <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
                 <Icon type="setting" />
               </Popover>
             </Tooltip>
           </Col>
         </Row>
       </Menu.Item>
-    	<SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-      		<MenuItemGroup title="Item 1">
-        		<Menu.Item key="1">Option 1</Menu.Item>
-        		<Menu.Item key="2">Option 2</Menu.Item>
-      		</MenuItemGroup>
-      		<MenuItemGroup title="Item 2">
-        		<Menu.Item key="3">Option 3</Menu.Item>
-        		<Menu.Item key="4">Option 4</Menu.Item>
-      		</MenuItemGroup>
-    	</SubMenu>
-    	<SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-      		<Menu.Item key="5">Option 5</Menu.Item>
-      		<Menu.Item key="6">Option 6</Menu.Item>
-      		<SubMenu key="sub3" title="Submenu">
-        		<Menu.Item key="7">Option 7</Menu.Item>
-        		<Menu.Item key="8">Option 8</Menu.Item>
-      		</SubMenu>
-    	</SubMenu>
-    	<SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
-      		<Menu.Item key="9">Option 9</Menu.Item>
-      		<Menu.Item key="10">Option 10</Menu.Item>
-      		<Menu.Item key="11">Option 11</Menu.Item>
-      		<Menu.Item key="12">Option 12</Menu.Item>
+    	<SubMenu key="folder1" title={<span><Icon type="folder" />文件夹1</span>}>
+        <Menu.Item key="1">
+          <Row>
+            <Col span={20}>
+              页面2
+            </Col>
+            <Col span={4}>
+              <Tooltip placement="top" title="设置页面的详细信息">
+                <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
+                  <Icon type="setting" />
+                </Popover>
+              </Tooltip>
+            </Col>
+          </Row>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Row>
+            <Col span={20}>
+              页面3
+            </Col>
+            <Col span={4}>
+              <Tooltip placement="top" title="设置页面的详细信息">
+                <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
+                  <Icon type="setting" />
+                </Popover>
+              </Tooltip>
+            </Col>
+          </Row>
+        </Menu.Item>
+
+        <SubMenu key="folder2" title={<span><Icon type="folder" />文件夹2</span>}>
+          <Menu.Item key="3">
+            <Row>
+              <Col span={20}>
+                页面4
+              </Col>
+              <Col span={4}>
+                <Tooltip placement="top" title="设置页面的详细信息">
+                  <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
+                    <Icon type="setting" />
+                  </Popover>
+                </Tooltip>
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Row>
+              <Col span={20}>
+                页面5
+              </Col>
+              <Col span={4}>
+                <Tooltip placement="top" title="设置页面的详细信息">
+                  <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
+                    <Icon type="setting" />
+                  </Popover>
+                </Tooltip>
+              </Col>
+            </Row>
+          </Menu.Item>
+        </SubMenu>
     	</SubMenu>
     </Menu>
 	);
