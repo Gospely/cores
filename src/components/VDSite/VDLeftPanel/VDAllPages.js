99734182d288d58a85cabcddd73220c91544141f
leinue <597055914@qq.com>
2017-02-09 11:42:15 +0800

    update

--- src/components/VDSite/VDLeftPanel/VDAllPages.js
+++ src/components/VDSite/VDLeftPanel/VDAllPages.js
@@ -15,6 +15,10 @@ import { Row, Col } from 'antd';
 
 const TabPane = Tabs.TabPane;
 
+import { Form, Input, Cascader, Select, Checkbox } from 'antd';
+const FormItem = Form.Item;
+const Option = Select.Option;
+
 const Component = (props) => {
 
 	const allPagesProps = {
@@ -29,41 +33,102 @@ const Component = (props) => {
 
 	}
 
+  const formItemLayout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const tailFormItemLayout = {
+    wrapperCol: {
+      span: 14,
+      offset: 0,
+    },
+  };
+
+  const config = {
+    rules: [{ type: 'object', required: true, message: 'Please select time!' }],
+  };
+  const rangeConfig = {
+    rules: [{ type: 'array', required: true, message: 'Please select time!' }],
+  };  
+
   const generatePageDetailSettings = () => {
+
+    var myStyle = {
+      width: parseInt($(document).width()) / 2,
+      height: parseInt($(document).height()) - 50
+    }
+
     return (
-      <div>
-        <p>Content</p>
-        <p>Content</p>
+      <div style={myStyle}>
+        <h2>SEO设置</h2>
+        <Form>
+          <FormItem
+            {...formItemLayout}
+            label={(
+              <span>
+                Title&nbsp;
+                <Tooltip title="建议页面单独地定义一个title标签，而不是重复的使用默认标题">
+                  <Icon type="question-circle-o" />
+                </Tooltip>
+              </span>
+            )}
+            hasFeedback
+          >
+            <Input />
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label={(
+              <span>
+                Description&nbsp;
+                <Tooltip title="Description用来描述网站，通常是比较通顺的一句话组成，不建议刻意堆积关键词，字数一般不超过100个汉字。">
+                  <Icon type="question-circle-o" />
+                </Tooltip>
+              </span>
+            )}
+            hasFeedback
+          >
+            <Input type="textarea" rows={4} />
+          </FormItem>
+        </Form>
+        <h2>自定义代码</h2>
+        <Form>
+          <FormItem
+            {...formItemLayout}
+            label="在<head>标签内"
+            hasFeedback
+          >
+            <Input type="textarea" rows={10} />
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="在</body>标签前"
+            hasFeedback
+          >
+            <Input type="textarea" rows={10} />
+          </FormItem>
+
+          <FormItem {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit" size="large">保存</Button>
+          </FormItem>
+        </Form>
       </div>
     );
   };
 
 	return (
-    <Menu onClick={allPagesProps.handlePageListItemClick}
-    	style={{ width: 240 }}
-    	defaultOpenKeys={['sub1']}
-    	selectedKeys={[props.vdpm.currentActivePageListItem]}
-    	mode="inline"
-    >
-      <Menu.Item key="index.html">
-        <Row>
-          <Col span={20}>
-            主页
-          </Col>
-          <Col span={4}>
-            <Tooltip placement="top" title="设置页面的详细信息">
-              <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
-                <Icon type="setting" />
-              </Popover>
-            </Tooltip>
-          </Col>
-        </Row>
-      </Menu.Item>
-    	<SubMenu key="folder1" title={<span><Icon type="folder" />文件夹1</span>}>
-        <Menu.Item key="1">
+    <div className="vd-allpages-list">
+      <Menu onClick={allPagesProps.handlePageListItemClick}
+      	style={{ width: '100%' }}
+      	defaultOpenKeys={['index.html']}
+      	selectedKeys={[props.vdpm.currentActivePageListItem]}
+      	mode="inline"
+      >
+        <Menu.Item key="index.html">
           <Row>
             <Col span={20}>
-              页面2
+              主页
             </Col>
             <Col span={4}>
               <Tooltip placement="top" title="设置页面的详细信息">
@@ -74,53 +139,69 @@ const Component = (props) => {
             </Col>
           </Row>
         </Menu.Item>
-        <Menu.Item key="2">
-          <Row>
-            <Col span={20}>
-              页面3
-            </Col>
-            <Col span={4}>
-              <Tooltip placement="top" title="设置页面的详细信息">
-                <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
-                  <Icon type="setting" />
-                </Popover>
-              </Tooltip>
-            </Col>
-          </Row>
-        </Menu.Item>
+      	<SubMenu key="folder1" title={<span><Icon type="folder" />文件夹1</span>}>
+          <Menu.Item key="1">
+            <Row>
+              <Col span={20}>
+                页面2
+              </Col>
+              <Col span={4}>
+                <Tooltip placement="top" title="设置页面的详细信息">
+                  <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
+                    <Icon type="setting" />
+                  </Popover>
+                </Tooltip>
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Row>
+              <Col span={20}>
+                页面3
+              </Col>
+              <Col span={4}>
+                <Tooltip placement="top" title="设置页面的详细信息">
+                  <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
+                    <Icon type="setting" />
+                  </Popover>
+                </Tooltip>
+              </Col>
+            </Row>
+          </Menu.Item>
 
-        <SubMenu key="folder2" title={<span><Icon type="folder" />文件夹2</span>}>
-          <Menu.Item key="3">
-            <Row>
-              <Col span={20}>
-                页面4
-              </Col>
-              <Col span={4}>
-                <Tooltip placement="top" title="设置页面的详细信息">
-                  <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
-                    <Icon type="setting" />
-                  </Popover>
-                </Tooltip>
-              </Col>
-            </Row>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Row>
-              <Col span={20}>
-                页面5
-              </Col>
-              <Col span={4}>
-                <Tooltip placement="top" title="设置页面的详细信息">
-                  <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
-                    <Icon type="setting" />
-                  </Popover>
-                </Tooltip>
-              </Col>
-            </Row>
-          </Menu.Item>
-        </SubMenu>
-    	</SubMenu>
-    </Menu>
+          <SubMenu key="folder2" title={<span><Icon type="folder" />文件夹2</span>}>
+            <Menu.Item key="3">
+              <Row>
+                <Col span={20}>
+                  页面4
+                </Col>
+                <Col span={4}>
+                  <Tooltip placement="top" title="设置页面的详细信息">
+                    <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
+                      <Icon type="setting" />
+                    </Popover>
+                  </Tooltip>
+                </Col>
+              </Row>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Row>
+                <Col span={20}>
+                  页面5
+                </Col>
+                <Col span={4}>
+                  <Tooltip placement="top" title="设置页面的详细信息">
+                    <Popover placement="right" title="设置页面的详细信息" content={generatePageDetailSettings()} trigger="click">
+                      <Icon type="setting" />
+                    </Popover>
+                  </Tooltip>
+                </Col>
+              </Row>
+            </Menu.Item>
+          </SubMenu>
+      	</SubMenu>
+      </Menu>
+    </div>
 	);
 
 };
