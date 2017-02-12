e429a548ec2c8105dd970543da70798ef17a8197
leinue <597055914@qq.com>
2017-02-12 16:07:14 +0800

    updae

--- src/models/vdsite/ModelVDPagesManager.js
+++ src/models/vdsite/ModelVDPagesManager.js
@@ -10,7 +10,50 @@ export default {
 			treeSelect: {
 				value: ''
 			}
-		}
+		},
+
+		pageList: [{
+			key: 'index.html',
+			name: '主页面',
+			seo: {
+				title: '主页面',
+				description: '介绍'
+			},
+			script: {
+				head: '',
+				body: ''
+			}
+		}, {
+			key: 'folder1',
+			name: '文件夹1',
+			children: [{
+				key: 'folder1/account.html',
+				name: '账户',
+				seo: {
+					title: '账户',
+					description: '介绍'
+				},
+				script: {
+					head: '',
+					body: ''
+				}
+			}, {
+				key: 'folder1/folder2',
+				name: '主页面',
+				children: [{
+					key: 'folder1/folder2/pw.html',
+					name: '忘记密码',
+					seo: {
+						title: '忘记密码',
+						description: '介绍'
+					},
+					script: {
+						head: '',
+						body: ''
+					}
+				}]
+			}]
+		}]
 	},
 
 	reducers: {
