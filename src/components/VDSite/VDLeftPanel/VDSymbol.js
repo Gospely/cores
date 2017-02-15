05cadf03e817cbd87e7935259423cf9bef0c0316
SharksevenRo <937257166@qq.com>
2017-02-15 17:42:12 +0800

    update

--- src/components/VDSite/VDLeftPanel/VDSymbol.js
+++ src/components/VDSite/VDLeftPanel/VDSymbol.js
@@ -1,7 +1,7 @@
 import React , {PropTypes} from 'react';
 import { connect } from 'dva';
 
-import { Button, Modal } from 'antd';
+import { Button, Modal, message, notification } from 'antd';
 import { Tabs, Icon } from 'antd';
 import { Tooltip } from 'antd';
 import { Row, Col } from 'antd';
@@ -11,32 +11,68 @@ const FormItem = Form.Item;
 
 const TabPane = Tabs.TabPane;
 
+const openNotificationWithIcon = (type, title, description) => (
+  notification[type]({
+    message: title,
+    description: description,
+  })
+);
 const Component = (props) => {
 
   const formItemLayout = {
     labelCol: { span: 8 },
     wrapperCol: { span: 16 }
-  };  
+  };
+  const formItemProps = {
 
+      chooseSymbol(e){
+
+         props.dispatch({
+             type: 'vdctrl/handleCurrentSymbolKey',
+             payload: e.key
+         });
+     },
+     checkSymbol(){
+         if(props.vdctrl.currentSymbolKey == '' || props.vdctrl.currentSymbolKey == null ){
+             openNotificationWithIcon('info', '请选择要填加到的部分');
+             return;
+         }
+     },
+     addSymbol(){
+         if(props.vdctrl.symbolName == '' || props.vdctrl.symbolName == null ){
+             openNotificationWithIcon('info', '控件名为空');
+             return;
+         }
+         props.dispatch({
+             type: 'vdctrl/addSymbol',
+         });
+     },
+     onChange(e){
+         props.dispatch({
+             type: 'vdctrl/handleSymbolNameChange',
+             payload: e.target.value
+         });
+     }
+  }
   const newSymbolsPopover = {
     content: (
       <Form className="form-no-margin-bottom">
         <FormItem {...formItemLayout} label="名称">
-          <Input size="small" />
+          <Input size="small"  value={props.vdctrl.symbolName} onChange={formItemProps.onChange}/>
         </FormItem>
-        <Button size="small">添加</Button>
+        <Button size="small" onClick={formItemProps.addSymbol} >添加</Button>
       </Form>
     )
   };
 
   const symbols = props.vdctrl.symbols.map((item, index) => {
     return (
-      <li className="ant-dropdown-menu-item" role="menuitem" key={index}>
-        <Row>
+      <li className="ant-dropdown-menu-item" role="menuitem" key={index} >
+        <Row onClick={formItemProps.chooseSymbol.bind(this, item)} >
           <Col span={4}>
             <svg width="26" height="26" viewBox="0 0 26 26" className="bem-Svg " style={{display: 'block', transform: 'translate(0px, 0px)', color: 'rgba(0, 0, 0, 0.521569)', flexShrink: '0'}}><path fill="currentColor" d="M13-.08l-.37.14L0,5V20.66l13,5.47,13-5.47V5ZM24,6.46l0-.06h0ZM13,10.29,2.64,6.15,13,2.07,23.32,6.15ZM2,6.4l0,.06V6.41ZM2,8.05l10,4v11.5L2,19.34ZM14,23.54V12l10-4V19.34Z"></path><polygon fill="currentColor" opacity=".3" points="1.71 19.59 12.8 24.16 12.98 11.34 1.62 6.76 1.71 19.59"></polygon></svg>
           </Col>
-          <Col span={18} style={{paddingLeft: '10px'}}>
+          <Col span={18} style={{paddingLeft: '10px'}} >
             <p>{item.name}</p>
             <p>{item.controllers.length} 个控件</p>
           </Col>
@@ -56,7 +92,7 @@ const Component = (props) => {
         <Col span={4} offset={20}>
           <Popover placement="right" trigger={['click']} content={newSymbolsPopover.content}>
               <Tooltip placement="bottom" title="选择一个块然后点击此按钮添加一个Symbol">
-                <Button style={{marginTop: '10px'}} shape="circle"><Icon type="plus" /></Button>                
+                <Button style={{marginTop: '10px'}} shape="circle"><Icon type="plus" onClick={formItemProps.checkSymbol}/></Button>
               </Tooltip>
           </Popover>
         </Col>
@@ -64,7 +100,7 @@ const Component = (props) => {
 
       <li style={{marginTop: '10px'}} className="ant-dropdown-menu-item-divider"></li>
 
-      <ul className="ant-dropdown-menu ant-dropdown-menu-vertical ant-dropdown-menu-light ant-dropdown-menu-root symbol-list" role="menu">
+      <ul className="ant-dropdown-menu ant-dropdown-menu-vertical ant-dropdown-menu-light ant-dropdown-menu-root symbol-list" role="menu" >
         {symbols}
       </ul>
 
--- src/models/vdsite/ModelVDCtrl.js
+++ src/models/vdsite/ModelVDCtrl.js
@@ -5,7 +5,7 @@ import { Icon } from 'antd';
 export default {
 	namespace: 'vdctrl',
 	state: {
-		
+
    		specialAttrList: ['custom-attr', 'link-setting', 'list-setting', 'heading-type', 'image-setting', 'select-setting', 'tabs-setting', 'slider-settings'],
 
 		publicAttrs: [{
@@ -67,7 +67,7 @@ export default {
 		}, {
 			title: '自定义属性',
 			key: 'custom-attr',
-			children: []		
+			children: []
 		}],
 
 		controllers: [{
@@ -128,7 +128,7 @@ export default {
 			name: "基础组件",
 			key: 'basic',
 			content: [{
-				icon: <svg width="36" height="36" viewBox="0 0 36 36" className="bem-Svg " style={{display: 'block', transform: 'translate(0px, 0px)'}}><path opacity=".2" fill="currentColor" d="M1 1h34v34H1z"></path><g fill="currentColor"><path d="M1 1h2v4H1zm32 0h2v4h-2zM3 1h2v2H3zm10 0h4v2h-4zM7 1h4v2H7zm12 0h4v2h-4zm-6 32h4v2h-4zm-6 0h4v2H7zm12 0h4v2h-4zm6-32h4v2h-4zm0 32h4v2h-4z"></path><path d="M31 1h4v2h-4zm2 30h2v4h-2zM1 31h2v4H1zm0-18h2v4H1zm0-6h2v4H1zm32 6h2v4h-2zM1 19h2v4H1zm32 0h2v4h-2zM1 25h2v4H1zm32 0h2v4h-2zm0-18h2v4h-2zm-2 26h2v2h-2z"></path><path d="M1 33h4v2H1z"></path></g><path opacity=".4" d="M35 1v34H1V1h34m1-1H0v36h36V0z"></path></svg>,				
+				icon: <svg width="36" height="36" viewBox="0 0 36 36" className="bem-Svg " style={{display: 'block', transform: 'translate(0px, 0px)'}}><path opacity=".2" fill="currentColor" d="M1 1h34v34H1z"></path><g fill="currentColor"><path d="M1 1h2v4H1zm32 0h2v4h-2zM3 1h2v2H3zm10 0h4v2h-4zM7 1h4v2H7zm12 0h4v2h-4zm-6 32h4v2h-4zm-6 0h4v2H7zm12 0h4v2h-4zm6-32h4v2h-4zm0 32h4v2h-4z"></path><path d="M31 1h4v2h-4zm2 30h2v4h-2zM1 31h2v4H1zm0-18h2v4H1zm0-6h2v4H1zm32 6h2v4h-2zM1 19h2v4H1zm32 0h2v4h-2zM1 25h2v4H1zm32 0h2v4h-2zm0-18h2v4h-2zm-2 26h2v2h-2z"></path><path d="M1 33h4v2H1z"></path></g><path opacity=".4" d="M35 1v34H1V1h34m1-1H0v36h36V0z"></path></svg>,
 				name: 'div块',
 				key: 'div-block',
 				details: {
@@ -148,7 +148,7 @@ export default {
 					}]
 				}
 			}, {
-				icon: <svg width="36" height="36" viewBox="0 0 36 36" className="bem-Svg " style={{display: 'block', transform: 'translate(0px, 0px)'}}><path opacity=".2" fill="currentColor" d="M1 1h34v34H1z"></path><g fill="currentColor"><path d="M1 1h2v4H1zm32 0h2v4h-2zM3 1h2v2H3zm10 0h4v2h-4zM7 1h4v2H7zm12 0h4v2h-4zm-6 32h4v2h-4zm-6 0h4v2H7zm12 0h4v2h-4zm6-32h4v2h-4zm0 32h4v2h-4z"></path><path d="M31 1h4v2h-4zm2 30h2v4h-2zM1 31h2v4H1zm0-18h2v4H1zm0-6h2v4H1zm32 6h2v4h-2zM1 19h2v4H1zm32 0h2v4h-2zM1 25h2v4H1zm32 0h2v4h-2zm0-18h2v4h-2zm-2 26h2v2h-2z"></path><path d="M1 33h4v2H1z"></path></g><path opacity=".4" d="M35 1v34H1V1h34m1-1H0v36h36V0z"></path><path opacity=".5" fill="currentColor" d="M13.6 27c-1.2 0-2.4-.5-3.2-1.3-.9-.9-1.3-2-1.3-3.2 0-1.2.5-2.4 1.3-3.2l3.2-3.2c.3-.3.7-.6 1-.8.7-.4 1.4-.6 2.2-.6 1.5 0 2.8.7 3.7 1.9l-1.3 1.3c-.5-.8-1.4-1.4-2.4-1.4-.2 0-.4 0-.6.1-.5.1-1 .4-1.3.7l-3.2 3.2c-.5.5-.8 1.2-.8 2 0 .7.3 1.4.8 2 .5.5 1.2.8 2 .8.7 0 1.4-.3 2-.8l.9-.9c.7.3 1.4.4 2.2.4L17 25.8c-1.1.7-2.2 1.2-3.4 1.2zm5.1-5.1c-.4 0-.7 0-1.1-.1-.8-.2-1.6-.6-2.2-1.2l-.5-.5 1.3-1.3c.1.2.3.4.4.5.5.5 1.2.8 2 .8.7 0 1.4-.3 2-.8l3.2-3.2c.5-.5.8-1.2.8-2 0-.7-.3-1.4-.8-2-.5-.5-1.2-.8-2-.8-.7 0-1.4.3-2 .8l-.7.9c-.7-.3-1.4-.4-2.2-.4l1.8-1.8c.9-.9 2-1.3 3.2-1.3 1.2 0 2.4.5 3.2 1.3.9.9 1.3 2 1.3 3.2s-.5 2.4-1.3 3.2l-3.2 3.2c-.3.3-.7.6-1 .8-.7.5-1.4.7-2.2.7z"></path></svg>,				
+				icon: <svg width="36" height="36" viewBox="0 0 36 36" className="bem-Svg " style={{display: 'block', transform: 'translate(0px, 0px)'}}><path opacity=".2" fill="currentColor" d="M1 1h34v34H1z"></path><g fill="currentColor"><path d="M1 1h2v4H1zm32 0h2v4h-2zM3 1h2v2H3zm10 0h4v2h-4zM7 1h4v2H7zm12 0h4v2h-4zm-6 32h4v2h-4zm-6 0h4v2H7zm12 0h4v2h-4zm6-32h4v2h-4zm0 32h4v2h-4z"></path><path d="M31 1h4v2h-4zm2 30h2v4h-2zM1 31h2v4H1zm0-18h2v4H1zm0-6h2v4H1zm32 6h2v4h-2zM1 19h2v4H1zm32 0h2v4h-2zM1 25h2v4H1zm32 0h2v4h-2zm0-18h2v4h-2zm-2 26h2v2h-2z"></path><path d="M1 33h4v2H1z"></path></g><path opacity=".4" d="M35 1v34H1V1h34m1-1H0v36h36V0z"></path><path opacity=".5" fill="currentColor" d="M13.6 27c-1.2 0-2.4-.5-3.2-1.3-.9-.9-1.3-2-1.3-3.2 0-1.2.5-2.4 1.3-3.2l3.2-3.2c.3-.3.7-.6 1-.8.7-.4 1.4-.6 2.2-.6 1.5 0 2.8.7 3.7 1.9l-1.3 1.3c-.5-.8-1.4-1.4-2.4-1.4-.2 0-.4 0-.6.1-.5.1-1 .4-1.3.7l-3.2 3.2c-.5.5-.8 1.2-.8 2 0 .7.3 1.4.8 2 .5.5 1.2.8 2 .8.7 0 1.4-.3 2-.8l.9-.9c.7.3 1.4.4 2.2.4L17 25.8c-1.1.7-2.2 1.2-3.4 1.2zm5.1-5.1c-.4 0-.7 0-1.1-.1-.8-.2-1.6-.6-2.2-1.2l-.5-.5 1.3-1.3c.1.2.3.4.4.5.5.5 1.2.8 2 .8.7 0 1.4-.3 2-.8l3.2-3.2c.5-.5.8-1.2.8-2 0-.7-.3-1.4-.8-2-.5-.5-1.2-.8-2-.8-.7 0-1.4.3-2 .8l-.7.9c-.7-.3-1.4-.4-2.2-.4l1.8-1.8c.9-.9 2-1.3 3.2-1.3 1.2 0 2.4.5 3.2 1.3.9.9 1.3 2 1.3 3.2s-.5 2.4-1.3 3.2l-3.2 3.2c-.3.3-.7.6-1 .8-.7.5-1.4.7-2.2.7z"></path></svg>,
 				name: '链接块',
 				key: 'link-block',
 				details: {
@@ -547,7 +547,7 @@ export default {
 							tag: 'label',
 							className: [],
 							attrs: []
-						}]					
+						}]
 					}],
 
 					children: [{
@@ -1199,12 +1199,14 @@ export default {
 		symbols: [{
 			name: '底部',
 			controllers: [],
-			key: '123'
+			key: 'bottom'
 		}, {
 			name: '头部',
 			controllers: [],
-			key: '234'
-		}]
+			key: 'head'
+		}],
+		currentSymbolKey: '',
+		symbolName: ''
 	},
 
 	subscriptions: {
@@ -1229,7 +1231,7 @@ export default {
 					content.map((ctrl, j) => {
 						for (var i = 0; i < state.publicAttrs.length; i++) {
 							const attrs = state.publicAttrs[i];
-							ctrl.details.attrs.push(attrs);						
+							ctrl.details.attrs.push(attrs);
 						};
 					});
 				});
@@ -1238,6 +1240,30 @@ export default {
 			push();
 
 			return {...state};
+		},
+		handleCurrentSymbolKey(state, { payload: key}) {
+
+			state.currentSymbolKey = key;
+			return { ...state};
+		},
+		handleSymbolNameChange(state, { payload: value}) {
+
+			console.log(value);
+			state.symbolName = value;
+			return { ...state};
+		},
+		addSymbol(state, { payload: value}){
+
+			for (var i = 0; i < state.symbols.length; i++) {
+				if(state.symbols[i].key == state.currentSymbolKey){
+					state.symbols[i].controllers.push({
+						name: state.symbolName,
+						key: state.symbolName,
+					})
+				}
+			}
+			state.symbolName = '';
+			return { ...state};
 		}
 
 	}
