80666efc651505ab509a897021a2a7cf46b86676
SharksevenRo <937257166@qq.com>
2017-03-14 19:19:27 +0800

    update

--- src/utils/initUIState.js
+++ src/utils/initUIState.js
@@ -1,71 +1,71 @@
 const initState = function (props, applicationId) {
 
-    const initStateOperate = {
-        'common':{
-            before: function(props, applicationId){
-
-            },
-            middle: function(props, applicationId){
-
-            },
-            after: function(props, applicationId){
-
-            }
-        },
-        'shell':{
-            before: function(props, applicationId){
-
-            },
-            middle: function(props, applicationId){
-
-            },
-            after: function(props, applicationId){
-
-            }
-        },
-        'ha':{
-            before: function(props, applicationId){
-
-            },
-            middle: function(props, applicationId){
-
-            },
-            after: function(props, applicationId){
-
-            }
-        },
-    }
-
     var UIState = JSON.parse(localStorage.UIState);
+    const initStateOperate = {
+        'common': function(){
+            props.dispatch({
+                type: 'sidebar/setActiveMenu',
+                payload: 'file'
+            });
+            props.dispatch({
+              type: 'devpanel/getConfig',
+              payload: { id : applicationId, UIState: UIState.UIState.devpanel}
+            });
 
+            props.dispatch({
+                type: 'devpanel/initPanel'
+            });
 
-    props.dispatch({
-        type: 'vdpm/initState',
-        payload: { UIState: UIState.UIState.vdpm }
-    });
-    console.log('vdCtrlTree/initState');
-    props.dispatch({
-        type: 'vdCtrlTree/initState',
-        payload: { UIState: UIState.UIState.vdCtrlTree }
-    });
-    console.log('vdstyles/initState');
-    props.dispatch({
-        type: 'vdstyles/initState',
-        payload: { UIState: UIState.UIState.vdstyles }
-    });
-    console.log('vdcore/initState');
-    props.dispatch({
-        type: 'vdcore/initState',
-        payload: { UIState: UIState.UIState.vdcore }
-    });
-    props.dispatch({
-        type: 'vdanimations/initState',
-        payload: { UIState: UIState.UIState.vdanimations }
-    });
+            if (!props.sidebar.appCreatingForm.fromGit) {
+                props.dispatch({
+                    type: 'file/fetchFileList'
+                });
+            }
+            props.dispatch({
+                type: 'file/initFiles',
+            });
+        },
+        'vd': function(){
 
-    localStorage.create = 'false';
-    localStorage.createPage = 'false';
+            props.dispatch({
+                type: 'vdpm/initState',
+                payload: { UIState: UIState.UIState.vdpm }
+            });
+            console.log('vdCtrlTree/initState');
+            props.dispatch({
+                type: 'vdCtrlTree/initState',
+                payload: { UIState: UIState.UIState.vdCtrlTree }
+            });
+            console.log('vdstyles/initState');
+            props.dispatch({
+                type: 'vdstyles/initState',
+                payload: { UIState: UIState.UIState.vdstyles }
+            });
+            console.log('vdcore/initState');
+            props.dispatch({
+                type: 'vdcore/initState',
+                payload: { UIState: UIState.UIState.vdcore }
+            });
+            props.dispatch({
+                type: 'vdanimations/initState',
+                payload: { UIState: UIState.UIState.vdanimations }
+            });
 
+            localStorage.create = 'false';
+            localStorage.createPage = 'false';
+            props.dispatch({
+                type: 'sidebar/setActiveMenu',
+                payload: 'vdsite-controllers'
+            });
+        }
+    };
+
+    
+    if(initStateOperate[localStorage.image.split(':')[0]]){
+        initStateOperate[localStorage.image.split(':')[0]]();
+    }else {
+        initStateOperate.common();
+    }
     if(UIState.UIState.previewer.loaded){
         props.dispatch({
             type: 'rightbar/setActiveMenu',
@@ -74,31 +74,11 @@ const initState = function (props, applicationId) {
         props.dispatch({
             type: 'index/toggleCommonPreviewer'
         });
-    }
-    props.dispatch({
-        type: 'cpre/initState',
-        payload: { UIState: UIState.UIState.previewer }
-    });
-
-    if(localStorage.image != 'vd:site') {
         props.dispatch({
-            type: 'sidebar/setActiveMenu',
-            payload: 'file'
-        });
-        props.dispatch({
-          type: 'devpanel/getConfig',
-          payload: { id : applicationId, UIState: UIState.UIState.devpanel}
-        });
-    }else {
-        props.dispatch({
-            type: 'sidebar/setActiveMenu',
-            payload: 'vdsite-controllers'
+            type: 'cpre/initState',
+            payload: { UIState: UIState.UIState.previewer }
         });
     }
-    props.dispatch({
-        type: 'UIState/setDySaveEffects'
-    });
-    localStorage.flashState = true;
 }
 
 export default initState;
