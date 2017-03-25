ada4c0825286b58ab193cfa8f44e3401f995b3d8
huchao <860931252@qq.com>
2017-03-25 15:24:45 +0800

    update

--- static/designer/vdsite/js/designer.js
+++ static/designer/vdsite/js/designer.js
@@ -1785,7 +1785,7 @@ $(function() {
 				console.log('listenContextmenu');
             	this.elem.contextmenu(function (e) {
             		e.preventDefault();
-            		//controllerOperations.select(jq(e.target).data('controller'));
+            		controllerOperations.select(jq(e.target).data('controller'));
             		controllerOperations.showRightClickMenu(e);
             	})
             },
